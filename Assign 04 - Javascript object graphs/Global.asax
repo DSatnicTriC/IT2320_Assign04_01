62a2efeb9849fff0d24e88c11d7ee549f6206c3e
achoo54321 <actionnotmotion@gmail.com>
2017-02-27 19:55:19 -0500

    Initial commit - empty stuff

---
+++ Assign 04 - Javascript object graphs/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Assign_04___Javascript_object_graphs.MvcApplication" Language="C#" %>
---
+++ Assign 04 - Javascript object graphs/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Assign_04___Javascript_object_graphs
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
